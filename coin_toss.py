534558444aa84a6cddec93e406dad1026ea376f0
curtisjacob53 <jjackson53@fingerlakes.edu>
2026-03-08 04:49:50 +0000

    coin flip

--- coin_toss.py
+++ coin_toss.py
@@ -1,15 +1,23 @@
 # FILE NAME - coin_toss.py
-# NAME: 
-# DATE: 
+# NAME: Jacob Jackson
+# DATE: 03/07/2026
 # BRIEF DESCRIPTION:  
 # 1. Make sure you fill out the comments above
 # 2. Write your code in the proper spot
 # 3. Be sure to answer the Reflection Questions and Attestation below
 # 4. The Sample Output has been included in this code for your convenience
 ########## ENTER YER CODE BELOW THIS LINE ##########
+import random
 
+print("===== Coin Flipper =====")
 
+number = random.randint(1, 100)
 
+if number >= 51: 
+   print("Tails")
+
+else:
+   print("Heads")
 
 
 
@@ -43,7 +51,7 @@ Tails
 '''
 
 1. What was the hardest part of completing this lab? 
-
+remembering the conditionals if and else
 
 
 
@@ -61,7 +69,7 @@ those concepts are required understanding for entry level programming. Reliance 
 like AI and internet sites like Chegg, CourseHero, StackOverflow, and general Google results
 may impede your understanding. Please rate how well you understand the concepts in this lab: 
 [ ] I understand very little about this lab.
-[ ] I am about 50/50 on this lab; I get parts of it but not the whole picture.
+[x] I am about 50/50 on this lab; I get parts of it but not the whole picture.
 [ ] I pretty much get it.
 [ ] I'm solid. Totally got it.
 '''
